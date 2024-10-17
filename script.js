39b5cad362c0b52f8d8f41a556ed8988674bc41e
xarlos12 <142985642+xarlos12@users.noreply.github.com>
2024-10-17 19:36:28 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let num = 25
+let yournumber
+prompt("enter the number in your mind : ")
+
+if(yournumber != num) {
+    num = prompt("enter the correct number you guess it incorrect👎 :")
+} else {
+    
+console.log("congratulation you entered it correct")
+}
